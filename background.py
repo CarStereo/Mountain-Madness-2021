b2f2b441593d046c9784690792787ac7078ab6b5
AkashMalhi1 <akashroopmalhi@gmail.com>
2021-02-13 23:42:27 +0000

    def functions

--- background.py
+++ background.py
@@ -1,8 +1,8 @@
 import pygame, sys, random 
 
 def sand_floor():
-  screen.blit(floor_surface,(floor_x_pos, 900))
-  screen.blit(floor_surface,(floor_surface_pos + 576, 900))
+	screen.blit(floor_surface,(floor_x_pos,900))
+	screen.blit(floor_surface,(floor_x_pos + 576,900))
 
 def obstacles():
   obstacles_position = random.choice(pipe_height)
