c15613da8f1968b824d28e705d3d4f24c64625a9
AkashMalhi1 <akashroopmalhi@gmail.com>
2021-02-13 23:01:18 +0000

    own variables

---
+++ background.py
@@ -0,0 +1,9 @@
+import pygame, sys, random 
+
+def sand_floor():
+  screen.blit(floor_surface,(floor_x_pos, 900))
+  screen.blit(floor_surface_pos + 576, 900))
+
+def obstacles():
+  obstacles_position = random.choice(pipe_height)
+  seaweed = seaweed_surface 
