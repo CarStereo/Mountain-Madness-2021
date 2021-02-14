9214633a80bebec0db7b10a8b69eec039813a019
Sunil Mann <sunilthemann@gmail.com>
2021-02-14 07:06:38 +0000

    Workingish

--- main.py
+++ main.py
@@ -5,11 +5,36 @@ import random, time
 import fish
 #import score
 
-def texts(score):
-  font = pygame.font.Font("GrinchedRegular.otf", 30)
-  scoretext = font.render("Score: "+str(score), 1,(255, 198, 0, 255))
-  scoreRect = scoretext.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/8))
-  screen.blit(scoretext, scoreRect)
+def menus(score, state):
+  font = pygame.font.Font("ASSETS/GrinchedRegular.otf", 30)
+  if state == 'main game':
+    scoreText = font.render("Score: "+str(score), 1,(255, 198, 0, 255))
+    scoreRect = scoreText.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/8))
+    screen.blit(scoreText, scoreRect)
+
+  elif state == 'game over':
+    scoreSurface = font.render("Score: "+str(score) ,1,(255, 198, 0, 255))
+    scoreRect = scoreSurface.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/8))
+    screen.blit(scoreSurface,scoreRect)
+    gameOverRect = gameOver.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+    screen.blit(gameOver, gameOverRect) 
+    highScoreSurf = font.render("High Score: "+str(HIGHSCORE),1,(255, 198, 0, 255))
+    highScoreRect = highScoreSurf.get_rect(center = (SCREEN_WIDTH/2,SCREEN_WIDTH/3))
+    screen.blit(highScoreSurf,highScoreRect)
+  else:
+    return
+
+def startmenu():
+  return
+
+def scoreUpdate(obs):
+  #obs is an array of the obstacles
+  #add points if an obstacle makes it past the fishes postion
+  for ob in obs:
+    if ob.centerx < SCREEN_WIDTH / 4 and ob.centerx > (SCREEN_WIDTH /4) - 10:
+      print("good job")
+      return 5
+  return 0
 
 def sand_floor():
 	screen.blit(ground,(floor_x_pos,900))
@@ -41,30 +66,34 @@ def removeObstacles(obs):
   return obs
 
 def checkCollision(obs):
-  global can_score
+  global death
   for ob in obs:
-    if fish_rect.colliderect(ob):
-      can_score = True
+    if P1.rect.colliderect(ob):
+      #print("you die")
+      death = True
       return False
 
-    if fish_rect.top <= -100 or fish_rect.bottom >= 900:
-      can_score = True
+    if P1.rect.top <= 0 or P1.rect.bottom >= 600:
+      #print("out of bounds")
+      death = True
       return False
     
-    return True
+  return True
 
+#do not know if we are going to implement
 def rotate_bird(fish):
-	new_fish = pygame.transform.rotozoom(fish,-fish_movement * 3,1)
-	return new_fish
+	P1 = pygame.transform.rotozoom(fish,-fish_movement * 3,1)
+	return P1
 
 def fish_animation():
-	new_fish = fish_frames[fish_index]
-	new_fish_rect = new_fish.get_rect(center = (100,fish_rect.centery))
-	return new_fish,new_fish_rect
+	P1 = fish_frames[fish_index]
+	new_P1.rect = P1.get_rect(center = (100,P1.rect.centery))
+	return P1,new_P1.rect
 
 #Initializing 
 pygame.init()
- 
+gameState = str('start menu')
+isAlive = True
 #Setting up FPS 
 FPS = 30
 FramePerSec = pygame.time.Clock()
@@ -73,8 +102,6 @@ FramePerSec = pygame.time.Clock()
 SCREEN_WIDTH = 450
 SCREEN_HEIGHT = 600
 SPEED = 5
-SCORE = 0
- 
 #setting up images
 background = pygame.image.load("ASSETS/background.png")
 background = pygame.transform.scale(background,(SCREEN_WIDTH,SCREEN_HEIGHT))
@@ -101,6 +128,22 @@ gravity = 5
 velocity = 0
 fish_movement = 0
 isJumping = False
+
+#score tracking
+SCORE = 0
+HIGHSCORE = 0
+inMenu = True
+while inMenu:
+  #start menu
+  for event in pygame.event.get():    
+        if event.type == QUIT:
+          pygame.quit()
+          sys.exit()    
+
+        if event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_SPACE:
+            inMenu = False   
+
 while True:
   #Cycles through all events occurring  
     for event in pygame.event.get():    
@@ -109,25 +152,42 @@ while True:
           sys.exit()
 
         if event.type == SPAWNHOOK:
-          hookList.extend(obstacles())
-          
+          hookList.extend(obstacles())       
+
         if event.type == pygame.KEYDOWN:
-          if event.key == pygame.K_SPACE and  not isJumping:
+          if event.key == pygame.K_SPACE and not isJumping:
+            gameState = 'main game'
             isJumping = True
           elif isJumping:
             isJumping = False
         if event.type == pygame.KEYUP:
           isJumping = False
+    
+    screen.blit(background, (0,0)) 
+    screen.blit(ground, (0,525))
+    
+    if isAlive: 
+      velocity = P1.jump(isJumping)
+      velocity += gravity
+      screen.blit(P1.image,P1.rect)
+      P1.rect.centery += velocity 
 
-    velocity = P1.jump(isJumping)
-    velocity += gravity
-    screen.blit(background, (0,0))     
-    texts(SCORE)
-    screen.blit(P1.image,P1.rect)
-    screen.blit(ground, (0,500))
-    hookList = moveObstacles(hookList)
-    hookList = removeObstacles(hookList)
-    drawObstacles(hookList)
-    P1.rect.centery += velocity
+      hookList = moveObstacles(hookList)
+      hookList = removeObstacles(hookList)
+      drawObstacles(hookList)
+      isAlive = checkCollision(hookList) 
+      newScore = scoreUpdate(hookList)
+      SCORE += newScore
+      
+      menus(SCORE,gameState)
+
+    else:
+      #bird died
+      gameState = 'game over'
+      if HIGHSCORE < SCORE:
+        HIGHSCORE = SCORE
+      menus(SCORE, gameState)
+  
     pygame.display.update()
     FramePerSec.tick(FPS)
+    
